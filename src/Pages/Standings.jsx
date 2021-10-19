7e41022af0faad1a6d39550e9e65cd1e4f4045ce
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2021-10-19 16:22:36 +0700

    new(Standings): create standings page

--- src/Pages/Standings.jsx
+++ src/Pages/Standings.jsx
@@ -1,7 +1,7 @@
 const Standings = () => {
   return (
     <div className="min-h-screen">
-      <h1 className="font-bold pt-24 text-center text-5xl">Klasemen</h1>
+      <h1 className="font-bold pt-24 text-center text-4xl">Klasemen</h1>
     </div>
   );
 };
