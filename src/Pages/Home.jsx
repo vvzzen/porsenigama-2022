0c7037ded9df24688299fb5ceebead559834b0a7
zen.com <ulel2123@gmail.com>
2021-10-24 19:28:52 +0700

    Responsive!

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { Title } from '../Components/Home'
 
 const Home = () => {
   return (
-    <div className="min-h-screen text-center gap-y-32 bg-krem">
+    <div className="md:min-h-screen text-center gap-y-32 bg-krem">
       <Title />
       <Section1 />
     </div>
