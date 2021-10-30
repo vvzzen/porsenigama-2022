8e0bf2c13576b720bae62872d2141e0de76c5829
vvzzen <84765458+vvzzen@users.noreply.github.com>
2021-10-30 20:56:56 +0700

    Navbar gaul gannnnn

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { useEffect, useState } from "react";
+import useScrollListener from "./hooks/useScrollListener.jsx";
 
 const Navbar = () => {
+  const [navClassList, setNavClassList] = useState([]);
+  const scroll = useScrollListener();
+
+  // update classList of nav on scroll
+  useEffect(() => {
+    const _classList = [];
+
+    if (scroll.y > 150 && scroll.y - scroll.lastY > 0)
+      _classList.push("nav-bar--hidden");
+
+    setNavClassList(_classList);
+  }, [scroll.y, scroll.lastY]);
+
   return (
-    <nav className="navbar flex justify-between  bg-red-700">
+    <nav className={navClassList.join(" ") + " navbar fixed z-100 w-full flex justify-between bg-red-700"}>
       <div className="flex justify-center align-center content-center ml-8">
         <img src="images/Title/logo porseni traced.svg" alt="" className="transform md:scale-110 lg:scale-150" />
       </div>
