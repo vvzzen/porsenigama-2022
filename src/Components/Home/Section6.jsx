e31d2ce196459cb95037fec7c76e4c69cce316de
evanjat2 <ann.jati2@gmail.com>
2021-11-02 01:07:15 +0700

    Update(Sec6) Firestore database

--- src/Components/Home/Section6.jsx
+++ src/Components/Home/Section6.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Jadwal from "./Jadwal";
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const Section6 = () => {
   return (
-    <>
     <div className="pt-10 relative bg-krem">
         <h2 className="hidden lg:block text-8xl font-nuku text-merah my-16"> Pertandingan Terdekat </h2>
         <div className="pb-20">
           <Jadwal />
         </div>
     </div>
-    </>
   );
+ 
 };
 
 export { Section6 };
