60a43f0f93ed4f07e5739eca6445d3f2829e6ba0
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2021-11-01 09:51:34 +0700

    add(Cabor): without responsiveness

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Navbar, Footer } from "./Components/Layout";
-import { Home, Standings, News, Faq } from "./Pages";
+import { Home, Standings, News, Faq, Cabor } from "./Pages";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
@@ -7,9 +7,26 @@ function App() {
     <div>
       <Navbar />
       <Switch>
-        <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home}></Route>
-        <Route path={`${process.env.PUBLIC_URL}/klasemen`} exact component={Standings} />
-        <Route path={`${process.env.PUBLIC_URL}/berita`} exact component={News} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/`}
+          exact
+          component={Home}
+        ></Route>
+        <Route
+          path={`${process.env.PUBLIC_URL}/klasemen`}
+          exact
+          component={Standings}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/cabor`}
+          exact
+          component={Cabor}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/berita`}
+          exact
+          component={News}
+        />
         <Route path={`${process.env.PUBLIC_URL}/faq`} exact component={Faq} />
       </Switch>
       <Footer />
---
+++ src/data/dataCabor.js
@@ -0,0 +1,28 @@
+const caborList = [
+  "Panahan",
+  "Panjat Tebing",
+  "Sepak Bola",
+  "Karate",
+  "Silat",
+  "Taekwondo",
+  "Badminton",
+  "Bridge",
+  "Catur",
+  "E-Sport",
+  "Judo",
+];
+
+const casenList = [
+  "Membaca Puisi",
+  "Monolog",
+  "Band",
+  "Melukis",
+  "Make up Creation",
+  "Keroncong",
+  "Vocal Group",
+  "Naskah Lakon",
+  "Fotografi",
+  "Seni Tari",
+];
+
+export { caborList, casenList };
