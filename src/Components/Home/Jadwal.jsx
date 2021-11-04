0b2dcd5fac180edf88c87fccdf553cf2035efc35
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2021-11-04 19:23:23 +0700

    update(homepage) filter schedule to only show upcoming match

--- src/Components/Home/Jadwal.jsx
+++ src/Components/Home/Jadwal.jsx
@@ -19,14 +19,16 @@ const Jadwal = () => {
     // const date = new Date();
     const subscriber = db
       .collection("dataJadwal")
-      .orderBy("time","asc")
+      .orderBy("time", "asc")
       // .where("time" ,">", date)
       .onSnapshot((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          getPostsFromFirebase.push({
-            ...doc.data(), //spread operator
-            key: doc.id, // `id` given to us by Firebase
-          });
+          if (doc.data().time.toDate() > new Date()) {
+            getPostsFromFirebase.push({
+              ...doc.data(), //spread operator
+              key: doc.id, // `id` given to us by Firebase
+            });
+          }
         });
         setPosts(getPostsFromFirebase);
       });
@@ -37,84 +39,86 @@ const Jadwal = () => {
 
   return (
     <div className="grid-cols-9 grid">
-      <button 
-          className ={"relative item-center w-12 bg-biru h-12 translate-x-1/2 rounded-full button"}
-          onClick={() => posts.carousel.slidePrev()}>
-          <img 
-              src={`${process.env.PUBLIC_URL}/images/Sec6/left-arrow.png`} 
-              style={{ 
-                  position:"absolute",
-                  top:"50%",
-                  left:"50%",
-                  transform: "translate(-50%,-50%)",
-                  height:"2rem"}}
-              alt="" />
+      <button
+        className={"relative item-center w-12 bg-biru h-12 translate-x-1/2 rounded-full button"}
+        onClick={() => posts.carousel.slidePrev()}>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/Sec6/left-arrow.png`}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+            height: "2rem"
+          }}
+          alt="" />
       </button>
 
       {posts.breakPoints = [
-          { width: 1, itemsToShow: 1 },
-          { width: 550, itemsToShow: 1 },
-          { width: 800, itemsToShow: 1 },
-          { width: 850, itemsToShow: 2 },
-          { width: 1450, itemsToShow: 2 },  
-        ],
-      
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 1 },
+        { width: 800, itemsToShow: 1 },
+        { width: 850, itemsToShow: 2 },
+        { width: 1450, itemsToShow: 2 },
+      ],
 
-      posts.length > 0 ? (
 
-        <Carousel className="col-span-7" breakPoints={posts.breakPoints} ref={ref => (posts.carousel = ref)} >
-        {posts.map((post) =>         
-          <div className="relative" key={post.key}>
-            <div className="relative mb-8">
-              <div className = {box}>
-                  <div className = {cont1}>
+        posts.length > 0 ? (
+
+          <Carousel className="col-span-7" breakPoints={posts.breakPoints} ref={ref => (posts.carousel = ref)} >
+            {posts.map((post) =>
+              <div className="relative" key={post.key}>
+                <div className="relative mb-8">
+                  <div className={box}>
+                    <div className={cont1}>
                       <div>
-                          <p className= "overflow-hidden text-sm lg:text-2xl font-nuku w-full text-center cabor">
-                              {post.cabor}
-                          </p>
+                        <p className="overflow-hidden text-sm lg:text-2xl font-nuku w-full text-center cabor">
+                          {post.cabor}
+                        </p>
                       </div>
+                    </div>
+                    <p className="font-bold font-sansPro text-lg mb-4">{post.category}</p>
+                    <div className={cont2}>
+                      <div className={cont3} >
+                        <p className="overflow-hidden text-md"> {post.party1} </p>
+                      </div>
+                      <div className="h-full grid content-evenly text-3xl font-nuku">
+                        <div className="vs"> VS </div>
+                      </div>
+                      <div className={cont3} >
+                        <p className="overflow-hidden text-md"> {post.party2} </p>
+                      </div>
+                    </div>
                   </div>
-                  <p className="font-bold font-sansPro text-lg mb-4">{post.category}</p>
-                  <div className = {cont2}> 
-                      <div className= {cont3 } >
-                          <p className="overflow-hidden text-md"> {post.party1} </p>
-                      </div>
-                      <div className ="h-full grid content-evenly text-3xl font-nuku">
-                          <div className="vs"> VS </div>
-                      </div>
-                      <div className= {cont3} >
-                          <p className="overflow-hidden text-md"> {post.party2} </p>
-                      </div>
-                  </div>
-              </div>
-              <div className='flex justify-center'>
-                  <div className="gradient border-white border -bottom-8 rounded-full bg-opacity-100 w-48 h-20 absolute grid text-center text-lg content-center font-sansPro">
+                  <div className='flex justify-center'>
+                    <div className="gradient border-white border -bottom-8 rounded-full bg-opacity-100 w-48 h-20 absolute grid text-center text-lg content-center font-sansPro">
                       <p className="text-merah"> {post.jam} </p>
                       <p className="text-biru"> {post.tanggal} </p>
                       <p className="text-merah"> {post.tempat} </p>
+                    </div>
                   </div>
-              </div>
-              </div>
-          </div>)
-        } 
-        </Carousel>
-        
-      ) : (
-        ""
-      )}
-      <button className={ "relative item-center w-12 bg-biru h-12 translate-x-1/2 rounded-full button"}  
-                        onClick={() => posts.carousel.slideNext() 
-                        }> 
-                        <img 
-                            src={`${process.env.PUBLIC_URL}/images/Sec6/right-arrow.png`} 
-                            style={{ 
-                                position:"absolute",
-                                top:"50%",
-                                left:"50%",
-                                transform: "translate(-50%,-50%)",
-                                height:"2rem"}}
-                            alt="" />
-                </button>
+                </div>
+              </div>)
+            }
+          </Carousel>
+
+        ) : (
+          ""
+        )}
+      <button className={"relative item-center w-12 bg-biru h-12 translate-x-1/2 rounded-full button"}
+        onClick={() => posts.carousel.slideNext()
+        }>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/Sec6/right-arrow.png`}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+            height: "2rem"
+          }}
+          alt="" />
+      </button>
     </div>
   );
 };
